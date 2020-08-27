2cc75b78d71f58578dc97114aa959168e4ed91f6
SAPBI <SAPBI@SAPBI-PC>
2020-08-27 09:11:38 +0100

    Second Push

--- JenkinsTest/src/Mycode.java
+++ JenkinsTest/src/Mycode.java
@@ -3,6 +3,9 @@ public class Mycode {
 
 	public static void main(String[] args) {
 		
+		for(int i=1;i<20; i++) {
+			System.out.println("Hurray......This code works" +i);
+		}
 		System.out.println("This is a sample program");
 
 	}
